c0cc371f3762f038e651f97eae1a35e27f488c36
PROF04 <PROF04@lab4-00>
2024-08-19 20:55:06 -0300

    Adicionado lógica dos componentes

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,35 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
 	
+	@FXML
+	private TextField txfUsuario;
+	@FXML
+	private PasswordField psfSenha;
+	@FXML
+	private Button btnAcessar;
+	@FXML
+	private Button btnCadastrar;
+	
+	@FXML
+	private void validarUsuario(ActionEvent event) {
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		String senhaUsuario;
+		senhaUsuario = psfSenha.getText();
+		
+		if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+			if(nomeUsuario.isEmpty()) {
+				//nome vazio
+			} else{
+				// Senha vazia
+			}
+		}
+	}
 }
--- LojaETEC/src/module-info.java
+++ LojaETEC/src/module-info.java
@@ -1,6 +1,7 @@
 module LojaETEC {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens br.com.etec.model to javafx.graphics, javafx.fxml;
 }
