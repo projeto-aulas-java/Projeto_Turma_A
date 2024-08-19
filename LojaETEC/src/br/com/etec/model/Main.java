d6b0daccff75002b25afd192356d6f46be52e8dc
PROF04 <PROF04@lab4-00>
2024-08-19 20:33:19 -0300

    Protótipo da tela de login.
    Adicionado campos de texto e botões.
    Sem verificação de acesso.

--- LojaETEC/src/br/com/etec/model/Main.java
+++ LojaETEC/src/br/com/etec/model/Main.java
@@ -2,20 +2,24 @@ package br.com.etec.model;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/etec/view/telaLogin.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/br/com/etec/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
